262c25d362ed7f4972965470308ea35cb37ce3f2
whitekite <1075790909@qq.com>
2025-05-13 21:56:04 +0800

    chore!: update ESM-only dist

---
+++ tsdown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  sourcemap: false,
+  clean: true,
+  dts: true,
+  target: 'node16',
+  treeshake: true,
+  format: ['esm'],
+  publint: true,
+  unused: true,
+})
